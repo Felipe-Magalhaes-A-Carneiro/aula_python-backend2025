d9b93e4f93a95d48df306a84aec91a6dfd860986
felipe senai <felipe@gmail.com>
2025-10-14 19:49:22 -0300

    realiza o desafio

--- calculo.py
+++ calculo.py
@@ -1,5 +1,8 @@
-def dobro(numero):
-    return int(numero) * 2
+def dobro(num):
+    return int(num) * 2
 
 def triplo(num):
     return int(num) * 3
+
+def quadruplo(num):
+    return int(num) * 4
--- main.py
+++ main.py
@@ -1,25 +1,64 @@
+# No menu tera opcoes como abaixo:
+# Digite a opção desejada:
+# 1- Calcular o dobro
+# 2- Calcular o triplo
+# 3- Calcular o quadrado
+# 4- Finalizar
+
+# Implemente tambem a nova funcao quadrado na biblioteca calculo
+
+# Mude o programa principal pra mostrar o menu em loop, e excute a 
+# função escolhida pelo usuario até que ele escolha 4 e finalize o programa
+
 # importando - modo 1
-# import calculo
+import calculo
 
 # importando - modo 2
 #from calculo import * # ' * ' quer dizer que você quer TODAS as funcoes
-from calculo import dobro, triplo # ' * ' quer dizer que você quer APENAS as funcoes dobro e triplo
+from calculo import dobro, triplo, quadruplo# ' * ' quer dizer que você quer APENAS as funcoes dobro e triplo
 
-print("Olá, Mundo! Agora no VScode!")
+print(""" === PROGRAMA DE CALCULO ===""")
 
-print("Programa dos Calculos:")
-print("-" * 50)
+inicializar = input("Digite 'y' para começar ou 'n' para sair.")
 
-x = input("Digite o numero e direi o dobro:")
+def menu():
+    while True:
+        print("""Escolha uma das opções abaixo:
+                1- Calcular o dobro
+                2- Calcular o triplo
+                3- Calcular o quadrado
+                4- Finalizar
+            """)
+        
+        opcao = input("Digite a opção desejada: ")
 
-dobro = dobro(x)
+        if opcao == "1":
+            print("1- Calcular o dobro")
+            numero = int(input("Digite o número que deseja ser dobrado: "))
 
-print(f"O dobro de '{x}' é: {dobro}")
+            dobro = calculo.dobro(numero)
+            print(f"O dobro de {numero} é: {dobro}")
+        
+        elif opcao == "2":
+            print("2- Calcular o triplo")
+            numero = int(input("Digite o número que deseja ter o seu triplo: "))
+            
+            triplo = calculo.triplo(numero)
+            print(f"O triplo de {numero} é: {triplo}")
+        
+        elif opcao == "3":
+            print("3- Calcular o quadrado")
+            numero = int(input("Digite o número que deseja ter o seu quadruplo "))
 
-# fazendo triplo: 
+            quadruplo = calculo.quadruplo(numero)
+            print(f"O quadruplo de {numero} é: {quadruplo}")
 
-y = input("Digite outro número e agora direi o seu triplo: ")
+        elif opcao == "4":
+            print("4- Finalizar. Finalizando... Obrigado. Volte sempre!")
+            break
 
-triplo = triplo(y)
+        else:
+            print("Digite um dos comandos disponíveis")
 
-print(f"O triplo de '{y}' é: {triplo}")
+if inicializar == "y":
+    menu()
