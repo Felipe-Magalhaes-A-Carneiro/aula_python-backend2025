83178cbfb574839b722339b5c1bec2ab6fa04e13
Felipe-SENAI <felipe@senai.com>
2025-10-16 21:48:10 -0300

    atualiza bibliteca

---
+++ ex_matplolib.py
@@ -0,0 +1,12 @@
+# pip install matplotlib = > 'pip install' é o termo usado no terminal para baixar uma biblioteca que não está no seu computador
+
+import matplotlib.pyplot as plt # estou chamando o módulo(.pyplot) da biblioteca
+
+vendas_meses = [1500, 1727, 1350, 999, 1050, 1027, 1022, 1500, 2000, 2362, 2100, 2762]
+meses = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez']
+
+plt.plot(meses, vendas_meses)
+plt.ylabel("Vendas")
+plt.xlabel("Meses")
+plt.axis([0, 12, 0, max(vendas_meses) + 500])
+plt.show()
